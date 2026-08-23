76c129d5662a42ee9f471f85475f76779e77ae21
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-23 17:06:19 -0400

    FIRED

--- employees/employee_character.gd
+++ employees/employee_character.gd
@@ -4,4 +4,14 @@ var application: Application
 
 func fired():
 	var tween1 = get_tree().create_tween()
-	tween1.tween_property(GameState.camera, "zoom", Vector2(5,5), 1.0).set_trans(Tween.TRANS_QUINT)
+	tween1.set_parallel()
+	tween1.set_trans(Tween.TRANS_CUBIC)
+	tween1.set_ease(Tween.EASE_OUT)
+	tween1.tween_property(GameState.camera, "zoom", Vector2(4.5,4.5), 1.0)
+	tween1.tween_property(GameState.camera, "global_position", global_position, 0.8)
+	await tween1.finished
+	var tween2 = get_tree().create_tween()
+	tween2.set_trans(Tween.TRANS_CUBIC)
+	tween2.set_parallel()
+	tween2.tween_property(GameState.camera, "zoom", Vector2(2.5,2.5), 0.3)
+	tween2.tween_property(GameState.camera, "global_position", GameState.camera.get_parent().global_position, 0.3)
