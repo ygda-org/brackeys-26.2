acd45052e2637cb31a0d4bec04520da58c1b66b7
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-23 17:08:23 -0400

    employee list in gamestate

--- employees/employee_character.gd
+++ employees/employee_character.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
+class_name Employee
 
 var application: Application
+
+func _ready():
+	GameState.employees_list.append(self)
 
 func fired():
 	var tween1 = get_tree().create_tween()
@@ -15,3 +19,6 @@ func fired():
 	tween2.set_parallel()
 	tween2.tween_property(GameState.camera, "zoom", Vector2(2.5,2.5), 0.3)
 	tween2.tween_property(GameState.camera, "global_position", GameState.camera.get_parent().global_position, 0.3)
+	await tween2.finished
+	GameState.employees_list.remove_at(GameState.employees_list.find(self))
+	queue_free()
--- globals/game_state.gd
+++ globals/game_state.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var camera: Camera2D
+
+## application accessible by Employee.application
+var employees_list: Array[Employee]
