98eb4e5653d73c5ec76744ecd4c951e306716e66
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-23 14:01:32 -0400

    base application

--- applications/base_application.gd
+++ applications/base_application.gd
@@ -1,4 +1,15 @@
 extends Resource
 class_name Application
 
+enum PastJobs {
+	PENGUIN
+}
+
+enum Skills {
+	PENGUIN
+}
+
 @export var person_name: String
+@export var past_jobs: Array[PastJobs]
+@export var skills: Array[Skills]
+@export var company_reason: String
