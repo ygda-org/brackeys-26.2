745b7866bda15c12f642b8b192384c221d93eb06
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-27 18:08:32 -0400

    player animations

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,8 @@ const ACCELERATION = 1500
 const DECELERATION = 3000
 @export var acceleration_curve: Curve
 
+var old_dir: Vector2 = Vector2(0,0)
+
 func _ready():
 	GameState.camera = $Camera2D
 
@@ -31,7 +33,24 @@ func _physics_process(delta):
 	move_and_slide()
 	$VisionArm.look_at(get_global_mouse_position())
 	$PunchHitbox.look_at(get_global_mouse_position())
-
+	var anim_string
+	var dir_save = dir
+	if not dir:
+		dir = old_dir
+	if dir.y > 0:
+		anim_string = "down"
+	elif dir.y < 0:
+		anim_string = "up"
+	elif dir.x > 0:
+		anim_string = "right"
+	elif dir.x < 0:
+		anim_string = "left"
+	if not anim_string:
+		return
+	if not dir_save:
+		anim_string += "_idle"
+	$Anim.play(anim_string)
+	old_dir = dir
 
 func _on_punch_hitbox_body_entered(body):
 	body.fired()
