d2e252b043145905d07a6ae68013e49bfcca9f84
Theophylaktos <91230260+theophylaktos@users.noreply.github.com>
2026-08-29 08:14:50 -0400

    so actualy i covercoompicatd all that for no reason

--- ui/company_handbook/company_handbook.gd
+++ ui/company_handbook/company_handbook.gd
@@ -8,19 +8,34 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_left_pressed():
-	if current_page <= 0:
-		return
-	current_page -= 1
-	$AnimationPlayer.play("flip_backwards")
-		
-func _on_right_pressed():
-	if current_page >= max_page:
-		return
-	current_page += 1
-	$AnimationPlayer.play("flip_forwards")
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#$Page/LeftPageContent.get_child(0).visible = false
+	#for i in $Page/LeftPageContent.get_child_count():
+		#$Page/LeftPageContent.get_child(i).visible = false
+		#
+	#match current_page:
+		#0: 
+			#$Page/LeftPageContent/PageZero.visible = true
+		#1: 
+			#$Page/LeftPageContent/PageOne.visible = true
+		#2: 
+			#$Page/LeftPageContent/PageTwo.visible = true
+			#
+#func _on_left_pressed():
+	#if current_page <= 0:
+		#return
+	#current_page -= 1
+	#$AnimationPlayer.play("flip_backwards")
+	#await $AnimationPlayer.animation_finished
+	#reset_scale()
+		#
+#func _on_right_pressed():
+	#if current_page >= max_page:
+		#return
+	#$AnimationPlayer.play("flip_forwards")
+	#await $AnimationPlayer.animation_finished
+	#current_page += 1
+	#
+#func reset_scale():
+	#$Page.scale = Vector2(1.0,1.0)
--- ui/papers/papers.gd
+++ ui/papers/papers.gd
@@ -2,6 +2,16 @@ extends Control
 
 var current_app : Application = null
 
+var portraits = {
+	1 : "uid://lkp81lwlj61h",
+	2 : "uid://dwynkhhna46jx",
+	3 : "uid://uyut62piu15r",
+	4 : "uid://b1hqjste1mkfw",
+	5 : "uid://bosottpmmxukb",
+	6 : "uid://cg23r42rk6aij",
+	7 : "uid://c4ubm2ecwpvr",
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -21,7 +31,7 @@ func load_application():
 	text += "College: " + str(current_app.college) + "\n"
 	text += "Company Reason: " + str(current_app.company_reason) + "\n"
 	$ApplicationText.text = text
-
+	$TextureRect.texture = load(portraits[randi_range(1,7)])
 
 func _on_hire_button_pressed() -> void:
 	GameState.hire()
