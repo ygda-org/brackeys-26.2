cfad1ebe1380b869e290dad75446f590ad749440
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-29 11:41:45 -0400

    fixed

--- player/player.gd
+++ player/player.gd
@@ -45,8 +45,8 @@ func _physics_process(delta):
 	move_and_slide()
 	$VisionArm.look_at(get_global_mouse_position())#rotation = lerpf($VisionArm.rotation, global_position.direction_to(get_global_mouse_position()).angle(), delta * 20)
 	$PunchHitbox.rotation = Vector2(Vector2i(1.3*global_position.direction_to(get_global_mouse_position()))).angle()
-	if dir != Vector2(0,0):
-		$VisionArm.rotation = lerpf($VisionArm.rotation, dir.angle(), delta * 20)
+	#if dir != Vector2(0,0):
+		#$VisionArm.rotation = lerpf($VisionArm.rotation, dir.angle(), delta * 20)
 	$PunchHitbox.look_at(get_global_mouse_position())
 	var anim_string
 	var dir_save = dir
